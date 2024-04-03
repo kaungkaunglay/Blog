99f682ea58a5c504987270097951ae3e4fc0ee62
Aung Khant Zin <aungkhantzin881@gmail.com>
2024-04-03 15:58:29 +0630

    Blog Starting

--- App.js
+++ App.js
@@ -1,20 +1,17 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+import React from 'react';
+import {createStackNavigator} from 'react-navigation-stack'
+import { createAppContainer } from 'react-navigation';
+import IndexScreen from './src/IndexScreen'
+import { BlogProvider } from './src/context/BlogContext';
+const navigator = createStackNavigator({
+  Index: IndexScreen
+}, {
+  initialRouteName: 'Index',
+  defaultNavigationOptions: {
+    title: 'Blogs'
+  }
 });
+const App =  createAppContainer(navigator);
+export default() => {
+  return <BlogProvider ><App/></BlogProvider>
+}
